90c9bdeffe49f069c09ca4d9dea87096b0b7d798
ifaxec <xecfahim.ahmed@gmail.com>
2024-05-26 23:45:29 +0100

    Updated app.js

--- app.js
+++ app.js
@@ -4,21 +4,35 @@ const questionSections = document.querySelectorAll('.question');
 
 let currentQuestionIndex = 0;
 
-nextBtn.addEventListener('click', () => {
+function showCurrentIndex() {
 
+    const questionIndexElement = document.querySelector(`.question[data-question-number="${currentQuestionIndex + 1}"] .question-index`);
+    console.log(questionIndexElement);
+    questionIndexElement.innerHTML = `Question ${currentQuestionIndex + 1} of ${questionSections.length}`;
+}
+
+
+function showCurrentQuestion() {
+    showCurrentIndex();
     questionSections.forEach((question, index) => {
-        currentQuestionIndex++;
         if (index === currentQuestionIndex) {
             question.classList.add('active-question');
         } else {
             question.classList.remove('active-question');
         }
+    });
+}
 
-        if (currentQuestionIndex > questionSections.length) {
-            currentQuestionIndex = 0;
-        }
-    })
+nextBtn.addEventListener('click', () => {
+    currentQuestionIndex++;
+    if (currentQuestionIndex >= questionSections.length) {
+        currentQuestionIndex = 0;
+    }
+    showCurrentQuestion();
+
 })
+
+showCurrentQuestion();
 
 
 
